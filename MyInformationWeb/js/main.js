443eaf138f61d95eefd285799953e79936e065da
mateuszgruba <mateusz.gruba.mg@gmail.com>
2018-06-18 13:10:31 +0200

    Code of my portfolioWeb. work only index.html to do: change load img background and add other pages

---
+++ MyInformationWeb/js/main.js
@@ -0,0 +1,100 @@
+var run =0;
+var opacity = 1;
+var currentX = 0;
+var currentY = 0;
+function initJSClass() {
+
+    changeImg();
+    changePlace();
+
+
+}
+// noinspection JSAnnotator
+// noinspection JSAnnotator
+
+function moveScreen() {
+
+
+}
+
+function changeImg() {
+
+
+    var mainBodyBackground = document.getElementById('HomeBody');
+    var imgUrls = [4];
+    imgUrls[0] = '../img/backgroundHD/forest.jpg';
+    imgUrls[1] = '../img/backgroundHD/greenForest.jpg';
+    imgUrls[2] = '../img/backgroundHD/redCloud.jpg';
+    imgUrls[3] = '../img/backgroundHD/waterfalls.jpg';
+
+
+    mainBodyBackground.style.backgroundImage = "url(" + imgUrls[run] + ")";
+    //mainBodyBackground.style.opacity = ''+opacity;
+
+
+
+    setTimeout(function () {
+
+        changeImg();
+
+        run ++;
+        if(run == 4){
+            run=0;
+        }
+        opacity -=0.1;
+        if (opacity < 0.1)
+        {
+            opacity = 1;
+        }
+
+    }, 7000);
+
+
+}
+function blurImg() {
+
+
+    var opacityEnd = opacity - 0.1;
+    var mainBodyBackground = document.getElementById('HomeBody');
+    mainBodyBackground.style.opacity = ''+opacityEnd;
+    setTimeout(function () {
+        blurImg();
+
+    },250)
+
+}
+function changePlace() {
+
+    var modifyDiv = document.getElementById('HomeBody');
+
+    var posX = window.getComputedStyle(modifyDiv).backgroundPositionX;
+    var posY = window.getComputedStyle(modifyDiv).backgroundPositionY;
+
+
+    var nextX = currentX;
+    var nextY = currentY;
+   // console.log("position Y: "+posY);
+    console.log("position X: "+posX);
+
+    if(currentX > 99)
+    {
+        currentX = 1;
+
+    }
+    if(currentY>99)
+    {
+        currentY =1;
+    }
+    posX = ''+nextX+'%';
+    posY = ''+nextY+'%';
+    modifyDiv.style.backgroundPositionX = posX;
+    modifyDiv.style.backgroundPositionY = posY;
+
+
+    setTimeout(function () {
+        changePlace();
+        currentY+=0.05;
+        currentX+=0.1;
+
+    },100);
+}
